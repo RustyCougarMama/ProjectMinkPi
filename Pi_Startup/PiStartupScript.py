8c7b982b0a27a5d4a8275a9e7e1c5a3be365f7c6
Christian Vindbæk <christianvf99@gmail.com>
2020-09-02 08:36:17 +0200

    edit to startupscript

--- Pi_Startup/PiStartupScript.py
+++ Pi_Startup/PiStartupScript.py
@@ -1,27 +1,13 @@
-<<<<<<< Updated upstream
+
 import os
 import time
 
 #time.sleep(15)
 dir = os.path.dirname(os.path.abspath(__file__))
-print(dir)
 dir = dir.replace('\\', '/')
-print(dir)
 os.system("sudo /sbin /ip link set can0 up type can bitrate 500000")
 print("this works somehow")
-os.system("python /Home/pi/ProjectMinkPi/main/Exchange.py sensorData" %dir)
+os.system("python /Home/pi/ProjectMinkPi/main/exchange.py sensorData" %dir)
 os.system("python /Home/pi/ProjectMinkPi/main/consumer-to-azure.py" %dir)
 os.system("python3 /Home/pi/ProjectMinkPi/AzureTesting/TestProducerWithCAN.py" %dir)
 exit(0)
-=======
-import os
-import time
-
-time.sleep(15)
-dir = os.path.dirname(os.path.abspath(__file__))
-print(dir)
-dir = dir.replace('\\', '/')
-print(dir)
-os.system("python %s/Exchange/PiExchange.py Temp" %dir)
-os.system("python %s/Producer/PiDummyProducer.py Temp" %dir)
->>>>>>> Stashed changes
